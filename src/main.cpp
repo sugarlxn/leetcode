61cb738df6f90effc81f826c471d52c9e45c8725
lxn_WSL <1933650668@qq.com>
2024-06-13 21:36:23 +0800

    添加一个模板链表类Linklist

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,154 @@ struct ListNode {
     ListNode(): val(0), next(NULL) {} //默认构造函数, 在为struct添加构造函数时，需要添加默认构造函数
 };
 
+namespace my_linklist{
+
+    template <typename T>
+    class Linklist
+    {
+    public:
+        struct LinkNode
+        {
+            T val;
+            LinkNode* next;
+            LinkNode();
+            LinkNode(T x);
+            ~LinkNode();
+        };
+
+        Linklist();
+        ~Linklist();
+        /// @brief 获取第index个节点的值，index从0开始，如果index大于链表长度，返回-1
+        /// @param index 
+        /// @return 
+        T get(int index);
+        /// @brief 在链表前面插入一个结点，val为结点的值
+        /// @param val 
+        bool addAtHead(T val);
+        /// @brief 在链表后面添加一个节点
+        /// @param val 
+        bool addAtTail(T val);
+        /// @brief 在index之前插入一个节点
+        /// @param index 
+        /// @param val 
+        bool addAtIndex(int index, T val);
+        /// @brief 删除第index个节点
+        /// @param index 
+        bool deleteAtIndex(int index);
+        /// @brief 打印链表
+        void printLinklist();
+
+    private:
+        int _size;
+        LinkNode * _dummyHead;
+
+    };
+
+    /// @brief Linklist的构造函数
+    /// @tparam T 
+    template <typename T>
+    Linklist<T>::Linklist()
+    {
+        _size = 0;
+        _dummyHead = new LinkNode();
+    }
+    
+    template <typename T>
+    Linklist<T>::~Linklist()
+    {
+    }
+
+    /// @brief LinkNode的构造函数
+    /// @tparam T 
+    template <typename T>
+    Linklist<T>::LinkNode::LinkNode()
+    {
+        val = 0;
+        next = nullptr;
+    }
+
+    template <typename T>
+    Linklist<T>::LinkNode::LinkNode(T x)
+    {
+        val = x;
+        next = nullptr;
+    }
+
+    template <typename T>
+    Linklist<T>::LinkNode::~LinkNode()
+    {
+    }   
+    
+    template <typename T>
+    T Linklist<T>::get(int index){
+        if (index > (_size -1) || index < 0){return -1;}
+        LinkNode * cur = _dummyHead->next;
+        while (index--){cur = cur->next;}
+        return cur->val;
+    }
+
+    template <typename T>
+    bool Linklist<T>::addAtHead(T val){
+        LinkNode * newNode = new LinkNode();
+        if(newNode==nullptr){return false;}
+        newNode->val = val;
+        newNode->next = _dummyHead->next;
+        _dummyHead->next = newNode;
+        _size++;
+        return true;
+    }
+
+    template <typename T>
+    bool Linklist<T>::addAtTail(T val){
+        LinkNode * newNode = new LinkNode();
+        if(newNode == nullptr){return false;}
+        newNode->val = val;
+        LinkNode * cur = _dummyHead;
+        while (cur->next != nullptr){cur = cur->next;}
+        cur->next = newNode;
+        _size++;
+        return true;
+    }
+
+    template <typename T>
+    bool Linklist<T>::addAtIndex(int index, T val){
+        if(index > _size){return false;}
+        LinkNode * newNode = new LinkNode();
+        if(newNode == nullptr){return false;}
+        newNode->val = val;
+        LinkNode * cur = _dummyHead;
+        while (index--){cur = cur->next;}
+        newNode->next = cur->next;
+        cur->next = newNode;
+        _size++;
+        return true;
+    }
+
+    template <typename T>
+    bool Linklist<T>::deleteAtIndex(int index){
+        if(index >= _size){return false;}
+        LinkNode * cur = _dummyHead;
+        while (index--){cur = cur->next;}
+        LinkNode * tmp = cur->next;
+        cur->next = cur->next->next;
+        delete tmp;
+        _size--;
+        return true;
+    }
+
+    template <typename T>
+    void Linklist<T>::printLinklist(){
+        LinkNode * cur = _dummyHead->next;
+        while (cur != nullptr){
+            std::cout << cur->val << " ";
+            cur = cur->next;
+        }
+        std::cout << std::endl;
+    }
+
+}
+
+
 vector<int> test1(vector<int>& nums){
     vector<int> res = nums;
     res[res.size()-1] = 8;
@@ -104,6 +252,23 @@ int main(int argc, char *argv[])
     boost::shared_ptr<int> p(new int(10));
     cout << *p << endl;
 
+    //创建一个int类型的链表，并加入元素 1 3 5 7 9
+    my_linklist::Linklist<int> linklist;
+    linklist.addAtHead(1);
+    linklist.addAtTail(3);
+    linklist.addAtIndex(1, 5);
+    linklist.addAtIndex(3, 7);
+    linklist.addAtIndex(4, 9);
+    linklist.printLinklist();
+    //删除第2个元素
+    linklist.deleteAtIndex(2);
+    linklist.printLinklist();
+    //删除第4个元素
+    linklist.deleteAtIndex(4);
+    linklist.printLinklist();
+    //添加10 到链表尾部
+    linklist.addAtTail(10);
+    linklist.printLinklist();
 
     return 0;
 
