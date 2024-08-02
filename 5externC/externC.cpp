af614f28feb730f535f500b6faa4a15368413df3
lxn_WSL <1933650668@qq.com>
2024-08-02 11:56:56 +0800

    Add 5externC project with extern "C" functions

---
+++ 5externC/externC.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "externCLib.hpp"
+using namespace std;
+
+
+int main(int argc, char * argv[]){
+    cout << "Hello World!" << endl;
+    printHelloWorld();
+    printHi();
+    printBye();
+    return 0;
+}
---
+++ 5externC/include/externCLib.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <cstdio>
+
+///NOTE{extern ＂C＂ 的运用可以简化“必须同时被 C 和 C++使用”的头文件维护工作。当这个文件用于C++时，
+///你希望含有 extern ＂C＂；用于 C 时，你不希望如此。由于预处理器（preprocessor）符号__cplusplus 只针对 C++才有定义，
+///所以这种“通用于数种语言”的头文件可以架构如下：}
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void printHelloWorld(){
+    printf("Hello World!\n");
+}
+void printHi(){
+    printf("Hi!\n");
+}
+void printBye(){
+    printf("Bye!\n");
+}
+
+#ifdef __cplusplus
+}
+#endif
