6a6c546c5b3e3e1cbedec65df5a01d3e2ed9c4f7
lxn_WSL <1933650668@qq.com>
2024-09-11 14:07:14 +0800

    Add max and min statistics to accumulator, specify the boost::accumulators::min function

--- 12boost_lib/accumulator1.cpp
+++ 12boost_lib/accumulator1.cpp
@@ -4,6 +4,8 @@
 #include <boost/accumulators/statistics/mean.hpp>
 #include <boost/accumulators/statistics/moment.hpp>
 #include <boost/accumulators/framework/features.hpp>
+#include <boost/accumulators/statistics/max.hpp>
+#include <boost/accumulators/statistics/min.hpp>
 #include <boost/version.hpp>
 
 
@@ -30,18 +32,17 @@ int main(int argc, char * argv[]){
     // add the features of tag::min
     //accumulator_set<double, features<tag::min, tag::mean>> acc_features;
     acc_feature(1.0);
-    acc_feature(2.0);
     acc_feature(3.0);
+    acc_feature(2.0);
 
     cout << "acc_feature mean = " << mean(acc_feature) << endl;
 
-    //FIXME: thsi block of code is not working, need to check, build error
-#if false
+    //NOTE: using the max and min feature, boost::accumulators::max and boost::accumulators::min
     accumulator_set<int, stats<tag::max, tag::min>> acc_test;
     acc_test(1);
     acc_test(2);
-    cout << "acc_test max=" << max(acc_test) << "acc_test min=" << min(acc_test) << endl;
-#endif
+    cout << "acc_test max=" << boost::accumulators::max(acc_test) << " " << "acc_test min=" << boost::accumulators::min(acc_test) << endl;
+
 
     std::cout << "Using Boost "     
           << BOOST_VERSION / 100000     << "."  // major version
