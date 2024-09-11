8f4be14441dc2bfc472210995d465dcce1180d1d
lxn_WSL <1933650668@qq.com>
2024-09-11 14:53:15 +0800

    Add boost accumulators features and use vector to store and accumulate data

--- 12boost_lib/accumulator1.cpp
+++ 12boost_lib/accumulator1.cpp
@@ -7,7 +7,8 @@
 #include <boost/accumulators/statistics/max.hpp>
 #include <boost/accumulators/statistics/min.hpp>
 #include <boost/version.hpp>
-
+#include <vector>
+#include <algorithm>
 
 using namespace boost::accumulators;
 using namespace std;
@@ -43,6 +44,22 @@ int main(int argc, char * argv[]){
     acc_test(2);
     cout << "acc_test max=" << boost::accumulators::max(acc_test) << " " << "acc_test min=" << boost::accumulators::min(acc_test) << endl;
 
+    //NOTE: using accumulators feature max and min
+    accumulator_set<double, boost::accumulators::features<tag::min, tag::max>> acc_test2;   
+    acc_test2(1.0);
+    acc_test2(2.0);
+    acc_test2(3.0);
+    cout << "acc_test2 max=" << boost::accumulators::max(acc_test2) << " " << "acc_test2 min=" << boost::accumulators::min(acc_test2) << endl;
+
+    //NOTE: using vector to store the data and accumulate the data
+    std::vector<double> data={1.0, 2.0, 3.0, 4.0, 5.0};
+    accumulator_set<double, features<tag::mean, tag::max>> acc_test3;
+    //NOTE: std::for_each is a function in algorithm header file
+    acc_test3 = std::for_each(data.begin(), data.end(), acc_test3);
+    cout << "acc_test3 mean=" << boost::accumulators::mean(acc_test3) << " " << "acc_test3 max=" << boost::accumulators::max(acc_test3) << endl;
+
+    //NOTE: Another way to extract the result from an accumulator_set
+    cout << "acc_test3 mean=" << boost::accumulators::extract_result<tag::mean>(acc_test3) << " " << "acc_test3 max=" << boost::accumulators::extract_result<tag::max>(acc_test3) << endl;
 
     std::cout << "Using Boost "     
           << BOOST_VERSION / 100000     << "."  // major version
