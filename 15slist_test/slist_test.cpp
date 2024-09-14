c4a93a6bdbdd4daa31452d881c7708e3f1986416
lxn_WSL <1933650668@qq.com>
2024-09-14 22:42:23 +0800

    Implement slist library

--- 15slist_test/slist_test.cpp
+++ 15slist_test/slist_test.cpp
@@ -2,6 +2,13 @@
 #include <algorithm>
 using namespace std;
 
+/*
+ 侯捷STL 4.9 slist 219页
+ slist 不是标准库中的东西，需要自己实现一套slist lib
+ #include <slist>
+
+*/
+
 int main(int argc, char * argv[]){
 
     cout << "hello world" << endl;
