1a3c8495da637bef2566de69ad1e6f5cf61e2284
lxn_WSL <1933650668@qq.com>
2024-09-29 18:49:21 +0800

    Fix hashtable declaration and remove unused code

--- 18test_hash/test_hash.cpp
+++ 18test_hash/test_hash.cpp
@@ -2,20 +2,21 @@
 #include <hash_set>
 #include <hash_map>
 #include <unordered_map>
-// hashtable decleared in this header file, c style  
-#include <hashtable.h>
+// // hashtable decleared in this header file, c style  
+// #include <hashtable.h>
 
 using namespace std;
 
 
 int main(int argc, char * argv[]){
 
-    cout<< "hello hash_set!" << endl;
-    //BUG: hashtable is not decleared in this scope
-    //hashtable<int,int,hash<int>,identity<int>,equal_to<int>,alloc> iht(50,hash<int>(),equal_to<int>());
+    // //BUG: hashtable is not decleared in this scope
+    // //hashtable<int,int,hash<int>,identity<int>,equal_to<int>,alloc> iht(50,hash<int>(),equal_to<int>());
 
-    //#FIXME reference to  hashtable.h 
-    _Hashtable<int,int,hash<int>,identity<int>,equal_to<int>,alloc> iht(50,hash<int>(),equal_to<int>());
+    // //#FIXME reference to  hashtable.h 
+    // _Hashtable<int,int,hash<int>,identity<int>,equal_to<int>,alloc> iht(50,hash<int>(),equal_to<int>());
+
+    cout<< "hello hash_set!" << endl;
 
     return 0;
 }
