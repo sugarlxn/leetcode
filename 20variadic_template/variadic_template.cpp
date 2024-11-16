588e97d72842267ebf582854a9d252ff4bfd4e92
lxn_WSL <1933650668@qq.com>
2024-11-16 19:11:10 +0800

    fix variadic template <(￣ c￣)y▂ξ

--- 20variadic_template/variadic_template.cpp
+++ 20variadic_template/variadic_template.cpp
@@ -1,3 +1,4 @@
+#pragma GCC optimize(2, 3, "Ofast", "inline")
 #include <iostream>
 #include <initializer_list>
 using namespace std;
@@ -10,7 +11,7 @@ void log(){
 //variadic template
 template<typename T, typename... Args>
 void log(T firstArg, Args... args){
-    cout << firstArg << endl;
+    cout << firstArg << " ";
     log(args...);
 }
 
@@ -30,6 +31,11 @@ int main(int argc, char * argv[]){
     log("a", 1, 2.0, 3.0f, "hello","good day today!");
 
     log2<int>({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    int i,j,m,n;
+    i=j=m=n=0;
+    cout << "begin" << endl;
+    log(i,j,m,n);
+    cout << "end" << endl;
 
     return 0;
 }
