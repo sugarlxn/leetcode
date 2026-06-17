d522ecf5761ea8c04dd9f08d8f4f0ad4be420c5e
lxn_WSL <1933650668@qq.com>
2026-06-17 16:49:42 +0800

    add some code about hashtable

--- 18test_hash/test_hash.cpp
+++ 18test_hash/test_hash.cpp
@@ -8,6 +8,24 @@
 using namespace std;
 
 
+//the implement of hash table 
+template<typename K, typename V>
+struct Node{
+    std::pair<const K, V> kv;
+    Node* next;
+    size_t cached_hash;
+    Node(const K& key, const V& value, size_t hash): kv(key, value), cached_hash(hash), next(nullptr) {}
+};
+
+//NOTE: HashTable 
+template<typename K, typename V>
+struct HashTable{
+    std::vector<Node<const K, V>*> buckets;
+    size_t bucket_count;
+    size_t size;
+
+};
+
 int main(int argc, char * argv[]){
 
     // //BUG: hashtable is not decleared in this scope
